11a4a42a647f22648ab7b02d38b0fa998df30533
Eshanshi Tyagi <65651755+Eshanshi@users.noreply.github.com>
2021-12-30 20:00:14 +0530

    Add files via upload

---
+++ models/Appointmentitem.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose");
+
+const foodreq=new mongoose.Schema({
+    doc_name:{
+        type:String,
+        required:true
+        
+    },
+    doc_profile:{
+        type:Number,
+        required:true
+    },
+    doc_appointment:{
+        type:Number,
+        required:true
+    },
+
+    appointment_status:{
+      type:String,
+      required:true
+    }
+})
+const appointment=new mongoose.model("AppointmentStatus",Appstat)
+module.exports=appointment;
